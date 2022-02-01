d64ca474a83eeb9098d693a90266e4d77cf900dd
Griffin Kelton <griffin.kelton@terradotta.com>
2022-02-01 14:28:49 -0500

    commit on 'master'

--- src/app/components/hexToRbg.component.ts
+++ src/app/components/hexToRbg.component.ts
@@ -1,31 +1,34 @@
-import { Component, OnInit, Input, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 
 // declare the javascript function here
 declare function calculateRatio(color1, color2): any;
 
 @Component({
   selector: 'constrast-ratio',
-  template: `
-<div data-ratio='' style='display:none;' #ratio></div>`
-
+  template: ` <div data-ratio="" style="display:none;" #ratio></div>`,
 })
-
 export class contRatio implements OnInit, AfterViewInit {
-@Input()
-fillColor?: string;
+  @Input()
+  fillColor?: string;
 
-textColor: string = '#ffffff'
+  textColor: string = '#ffffff';
 
-@ViewChild('ratio') input: ElementRef;
-dataRatio: string;
+  @ViewChild('ratio') input: ElementRef;
+  dataRatio: number;
 
-ngOnInit() {
-    calculateRatio(this.fillColor,this.textColor); // function call 
+  ngOnInit() {
+    calculateRatio(this.fillColor, this.textColor); // function call
   }
 
-ngAfterViewInit(): void {
-    this.dataRatio = this.input.nativeElement.getAttribute("data-ratio");
-    console.log(this.dataRatio)
-}
-
+  ngAfterViewInit(): void {
+    this.dataRatio = this.input.nativeElement.getAttribute('data-ratio');
+    console.log(this.dataRatio);
+  }
 }
