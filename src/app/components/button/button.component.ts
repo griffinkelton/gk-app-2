32b9e71e366f4c65179ecd81089b93f41abaf0f3
Griffin Kelton <griffin.kelton@terradotta.com>
2022-02-07 09:42:58 -0500

    commit on 'master'

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -3,15 +3,15 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'storybook-button',
   template: ` <button
-    type='button'
-    (click)='onClick.emit($event)'
-    [ngClass]='classes'>
-    <span [ngClass]='[icon]'></span> {{ label }}
+    type="button"
+    (click)="onClick.emit($event)"
+    [ngClass]="classes"
+  >
+    <span [ngClass]="[icon]"></span> {{ label }}
   </button>`,
   styleUrls: ['./button.css'],
 })
 export class sbButtonComponent {
-  
   /**
    * How large should the button be?
    */
@@ -36,7 +36,7 @@ export class sbButtonComponent {
   onClick = new EventEmitter<Event>();
 
   public get classes(): string[] {
-    const mode = 'storybook-button'
+    const mode = 'storybook-button';
 
     return ['storybook-button', `storybook-button--${this.size}`, mode];
   }
